e5ca8866713354792964f3103e04d60b85e06b75
DevSamaa <samaakanani@gmail.com>
2020-07-29 12:32:47 +1000

    initial commit

---
+++ MastermindKata/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MastermindKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //this is going to be the mastermind kata
+        }
+    }
+}
