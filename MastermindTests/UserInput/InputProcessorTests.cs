a86761884ecacda6cafb447952ea515e23f55eee
DevSamaa <samaakanani@gmail.com>
2020-08-18 18:33:18 +1000

    changed order of InputProcessTests

--- MastermindTests/UserInput/InputProcessorTests.cs
+++ MastermindTests/UserInput/InputProcessorTests.cs
@@ -13,6 +13,39 @@ namespace MastermindTests.UserInput
              _inputProcessor = new InputProcessor(); 
         }
         
+        [Theory]
+        [Trait("Category","Unit")]
+        [InlineData("RED,blUe,green,yellOW")]
+        [InlineData("RED    ,blUe,   green,  yellOW")]
+        [InlineData("RED   ,BLUE,GREEN,  YELLOW")]
+        public void ProcessInput_ShouldReturnLowerCaseStringArrayWithoutWhiteSpace(string userInput)
+        {
+            var actualResult = _inputProcessor.ProcessInput(userInput);
+            var expectedResult = new string[]{"red", "blue", "green", "yellow"};
+            Assert.Equal(expectedResult, actualResult);
+        }
+        
+        
+        [Theory]
+        [Trait("Category","Unit")]
+        [InlineData("blue, red, green, orange",
+            "blue,red,green,orange")]
+        
+        [InlineData("blue,  red,  green,  orange", 
+            "blue,red,green,orange")]
+        
+        [InlineData("blue,  red,        green,      orange", 
+            "blue,red,green,orange")]
+        
+        [InlineData("anything,              could,   go,    in, here", 
+            "anything,could,go,in,here")]
+        public void RemoveWhiteSpace_ShouldReturnStringWithoutWhiteSpace(string userInput, string expectedResult)
+        {
+            var actualResult = _inputProcessor.RemoveWhiteSpace(userInput);
+            Assert.Equal(expectedResult, actualResult);
+        }
+        
+        
         public static IEnumerable<object[]> InputData()
         {
             yield return new object[]
@@ -41,7 +74,6 @@ namespace MastermindTests.UserInput
                 new string[] {"1","2","6","8"}
             };
         }
-        
         [Theory]
         [Trait("Category","Unit")]
         [MemberData(nameof(InputData))]
@@ -49,42 +81,6 @@ namespace MastermindTests.UserInput
         {
             var actualResult = _inputProcessor.PutUserGuessIntoArray(userInput);
             Assert.Equal(expectedResult,actualResult);
-        }
-        
-
-        [Theory]
-        [Trait("Category","Unit")]
-        [InlineData("blue, red, green, orange",
-            "blue,red,green,orange")]
-        
-        [InlineData("blue,  red,  green,  orange", 
-            "blue,red,green,orange")]
-        
-        [InlineData("blue,  red,        green,      orange", 
-            "blue,red,green,orange")]
-        
-        [InlineData("anything,              could,   go,    in, here", 
-            "anything,could,go,in,here")]
-        public void RemoveWhiteSpace_ShouldReturnStringWithoutWhiteSpace(string userInput, string expectedResult)
-        {
-            var actualResult = _inputProcessor.RemoveWhiteSpace(userInput);
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        
-        
-        [Theory]
-        [Trait("Category","Unit")]
-        
-        [InlineData("RED,blUe,green,yellOW")]
-        [InlineData("RED    ,blUe,   green,  yellOW")]
-        [InlineData("RED   ,BLUE,GREEN,  YELLOW")]
-
-        public void ProcessInput_ShouldReturnLowerCaseStringArrayWithoutWhiteSpace(string userInput)
-        {
-            var actualResult = _inputProcessor.ProcessInput(userInput);
-            var expectedResult = new string[]{"red", "blue", "green", "yellow"};
-            Assert.Equal(expectedResult, actualResult);
         }
         
         }
