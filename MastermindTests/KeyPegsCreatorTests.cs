0f227db3b48c2f5607cbb407bd89d3b5ba5edbd7
DevSamaa <samaakanani@gmail.com>
2020-08-18 23:10:20 +1000

    KeyPegsCreatorTest: commented out test that might be reduantant,added more examples to other test

--- MastermindTests/KeyPegsCreatorTests.cs
+++ MastermindTests/KeyPegsCreatorTests.cs
@@ -8,49 +8,73 @@ namespace MastermindTests
 {
     public class KeyPegsCreatorTests
     {
-        
         public static IEnumerable<object[]> InputData()
             {
                 yield return new object[] { 
-                    new string[] {"Yellow", "Blue", "Yellow", "Red"},
+                    new string[] {"red", "blue", "green", "purple"},
+                    new string[] {"yellow", "blue", "yellow", "red"},
                     new List<string>() {"Black", "White"} };
+                
                 yield return new object[] { 
-                    new string[] {"Blue", "Red", "Purple","Green"},
+                    new string[] {"red", "blue", "green", "purple"},
+                    new string[] {"blue", "red", "purple","green"},
                     new List<string>(){"White", "White", "White", "White"} };
+                
                 yield return new object[] { 
-                    new string[] {"Red", "Blue", "Green", "Purple"},
+                    new string[] {"red", "blue", "green", "purple"},
+                    new string[] {"red", "blue", "green", "purple"},
                     new List<string>(){"Black", "Black", "Black", "Black"} };
+                
                 yield return new object[] { 
-                    new string[] {"Red", "Orange", "Blue", "Blue"},
+                    new string[] {"red", "blue", "green", "purple"},
+                    new string[] {"red", "orange", "blue", "blue"},
                     new List<string>(){"Black", "White"} };
+                
+                yield return new object[] { 
+                    new string[] {"purple", "orange", "blue", "green"},
+                    new string[] {"orange", "purple", "blue","green"},
+                    new List<string>(){"Black", "Black", "White", "White"} };
+                
+                yield return new object[] { 
+                    new string[] {"red", "blue", "green", "purple"},
+                    new string[] {"yellow", "orange", "blue", "blue"},
+                    new List<string>(){ "White"} };
+                
+                yield return new object[] { 
+                    new string[] {"red", "blue", "green", "purple"},
+                    new string[] {"yellow", "yellow", "yellow", "yellow"},
+                    new List<string>(){} };
+                
+                yield return new object[] { 
+                    new string[] {"orange", "orange", "orange", "orange"},
+                    new string[] {"blue", "green", "yellow", "purple"},
+                    new List<string>(){} };
             }
         
         
         [Theory]
+        [Trait("Category","Unit")]
         [MemberData(nameof(InputData))]
-        public void GenerateShouldReturnExpectedArrays(string[] userPegs, string[] expectedResult)
+        public void Generate_ShouldReturnExpectedArray(string[]codePegs, string[] userPegs, string[] expectedResult)
         {
-            var codePegs = new string[] {"Red", "Blue", "Green", "Purple"};
             var keyPegsCreator =new KeyPegsCreator();
-
             var actualResult =keyPegsCreator.Generate(codePegs,userPegs);
             
             Assert.Equal(expectedResult, actualResult);
         }
 
-        [Fact]
-        public void AllWrongAnswersShouldReturnEmptyList()
-        {
-            var codePegs = new string[] {"Red", "Blue", "Green", "Purple"};
-            var userPegs = new string[] {"Orange", "Orange", "Orange", "Orange"};
-
-            var keyPegsCreator =new KeyPegsCreator();
-            var actualResult =keyPegsCreator.Generate(codePegs,userPegs);
-            
-            Assert.True(actualResult.IsNullOrEmpty());
-            Assert.Empty(actualResult);
-            
-        }
+        // [Fact]
+        // public void Generate_GivenAllWrongAnswers_ShouldReturnEmptyList()
+        // {
+        //     var codePegs = new string[] {"Red", "Blue", "Green", "Purple"};
+        //     var userPegs = new string[] {"Orange", "Orange", "Orange", "Orange"};
+        //
+        //     var keyPegsCreator =new KeyPegsCreator();
+        //     var actualResult =keyPegsCreator.Generate(codePegs,userPegs);
+        //     
+        //     Assert.Empty(actualResult);
+        //     
+        // }
     }
 }
 
